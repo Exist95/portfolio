a78a2cb5794dfd06b5fac6a80de560f96d7aae34
Exist95 <jyeol0210@naver.com>
2022-12-16 09:25:52 +0900

    아이콘 변경 및 필요없는 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BackGround>
+      <Header>
+        <div>종열</div>
+        <Menu>
+          <div>홈</div>
+          <div>소개</div>
+          <div>프로젝트</div>
+          <div>연락하기</div>
+        </Menu>
+      </Header>
+      <Ex>asdasd</Ex>
+    </BackGround>
   );
 }
+
+const BackGround = styled.div`
+  background-color: #282c34;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.25);
+  width: 100%;
+  color: white;
+  padding: 20px 0px 20px 20px;
+  font-size: 20px;
+  > div:nth-child(1) {
+    cursor: pointer;
+    &:hover {
+      color: red;
+    }
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  gap: 20px;
+  padding-right: 40px;
+  > div {
+    cursor: pointer;
+    &:hover {
+      color: pink;
+    }
+  }
+`;
+
+const Ex = styled.div`
+  background-color: green;
+`;
 
 export default App;
