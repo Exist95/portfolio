8ce58af27ad1d2d304fb3692df518644e977fafa
노종열 <96723716+Exist95@users.noreply.github.com>
2023-01-15 15:22:22 +0900

    Delete Test.js

--- src/project/Test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SectionWrapper from "../components/global/SectionWrapper";
-
-const test = () => {
-  return <SectionWrapper id="test">test</SectionWrapper>;
-};
-
-export default test;
